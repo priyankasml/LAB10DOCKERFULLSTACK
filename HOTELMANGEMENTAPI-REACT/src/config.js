b313af51fa0e8a06a76b9002d773771d5b23a8c7
unknown <priyankasml54@gmail.com>
2025-10-17 22:31:55 +0530

    first commit

--- HOTELMANGEMENTAPI-REACT/src/config.js
+++ HOTELMANGEMENTAPI-REACT/src/config.js
@@ -1,2 +1,2 @@
-// Centralized API URL
-export const API_URL = "http://localhost:8080/api/hotels";
+// Centralized API URL for React frontend
+export const API_URL = "http://localhost:2025/api/hotels";
