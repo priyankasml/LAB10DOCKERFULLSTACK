eb76115fecd63f4a7f6ef909d605a7b5abb9d2b3
unknown <priyankasml54@gmail.com>
2025-10-17 20:12:54 +0530

    intail commit

---
+++ HOTELMANAGEMENT-SPRINGBOOT/src/main/java/com/klef/dev/HotelmanagementSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.klef.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelmanagementSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelmanagementSpringbootApplication.class, args);
+		System.out.print("Project running successfully");
+	}
+
+}
---
+++ HOTELMANGEMENTAPI-REACT/src/config.js
@@ -0,0 +1,2 @@
+// Centralized API URL
+export const API_URL = "http://localhost:8080/api/hotels";
